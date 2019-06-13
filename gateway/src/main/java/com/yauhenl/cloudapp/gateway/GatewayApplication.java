3f98380119b7b2df348942d7d4b8869ec682376a
yauhenl <yauhen.lisimau@gmail.com>
2019-06-13 12:04:49 +0300

    auth app

--- gateway/src/main/java/com/yauhenl/cloudapp/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/yauhenl/cloudapp/gateway/GatewayApplication.java
@@ -1,4 +1,4 @@
-package com.yauhenl.cloudapp.discovery;
+package com.yauhenl.cloudapp.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
